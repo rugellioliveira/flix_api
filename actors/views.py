eb76a1430be144bb6044f89abf7d71343b7bb2c5
rugelli <rugelli.oliveira@gmail.com>
2024-07-12 23:35:39 -0300

    first commit

---
+++ actors/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from actors.models import Actor
+from rest_framework.permissions import IsAuthenticated
+from actors.serializers import ActorSerializer
+from app.permissions import GlobalDefaultPermission
+
+
+# Create your views here.
+class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
---
+++ genres/views.py
@@ -0,0 +1,18 @@
+from genres.models import Genre
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from genres.serializers import GenreSerializer
+from app.permissions import GlobalDefaultPermission
+
+
+# Create your views here.
+class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
